285252061ded1362f745601cce6c60073160e70a
Cloudless67 <cloudlesslakitu67@gmail.com>
2020-07-28 03:15:08 +0900

    db connect pool

--- middleware/mysql.js
+++ middleware/mysql.js
@@ -1,19 +1,11 @@
 var mysql = require('mysql')
 
 module.exports = function(host, user, password, database){
-  console.log('host: ' + host)
-  console.log('user: ' + user)
-  console.log('password: ' + password)
-  console.log('database: ' + database)
-  var connection = mysql.createConnection({
+  var connection = mysql.createPool({
     host     : host,
     user     : user,
     password : password,
     database : database
-  });
-  connection.connect(function(err) {
-      if (err) throw err;
-      console.log('Connected to database.')
   });
   return connection
 }
