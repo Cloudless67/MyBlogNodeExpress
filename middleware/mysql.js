d029168ea4651a574dd4d4e2fab12d470974221d
Cloudless67 <cloudlesslakitu67@gmail.com>
2020-07-27 22:54:39 +0900

    db connect2

--- middleware/mysql.js
+++ middleware/mysql.js
@@ -1,6 +1,10 @@
 var mysql = require('mysql')
 
 module.exports = function(host, user, password, database){
+  console.log('host: ' + host)
+  console.log('user: ' + user)
+  console.log('password: ' + password)
+  console.log('database: ' + database)
   var connection = mysql.createConnection({
     host     : host,
     user     : user,
@@ -9,7 +13,7 @@ module.exports = function(host, user, password, database){
   });
   connection.connect(function(err) {
       if (err) throw err;
+      console.log('Connected to database.')
   });
-  console.log('Connected to database.')
   return connection
 }
