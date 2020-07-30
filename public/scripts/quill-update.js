a7c5a89a0d8ee75bd37cc68408ee0523b3078adb
Cloudless67 <cloudlesslakitu67@gmail.com>
2020-07-30 21:24:42 +0900

    favicon & quill

---
+++ public/scripts/quill-update.js
@@ -0,0 +1,38 @@
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block', 'image'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+
+  [{ 'color': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }]
+];
+
+var quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: toolbarOptions
+  },
+  placeholder: '본문',
+  theme: 'snow'  // or 'bubble'
+});
+
+var v = quill.getContents().ops[0].insert;
+alert(v);
+if(v){
+  quill.setContents(JSON.parse(v));
+}
+
+var form = document.querySelector('form');
+form.onsubmit = function() {
+  // Populate hidden form on submit
+  var about = document.querySelector('input[name=body]');
+  about.value = JSON.stringify(quill.getContents());
+  console.log(about.value)
+};
