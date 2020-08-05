e122e290aa2948a0ad03e8b0f5036b7b84c1abe6
Cloudless67 <cloudlesslakitu67@gmail.com>
2020-08-05 15:44:33 +0900

    image upload

--- public/scripts/quill-write.js
+++ public/scripts/quill-write.js
@@ -15,6 +15,7 @@ var toolbarOptions = [
   [{ 'align': [] }]
 ];
 
+// Create Quill instance
 var quill = new Quill('#editor-container', {
   modules: {
     toolbar: toolbarOptions
@@ -22,6 +23,81 @@ var quill = new Quill('#editor-container', {
   placeholder: '본문',
   theme: 'snow'  // or 'bubble'
 });
+
+// Set contents if update
+var content = document.getElementById('editor-container').getAttribute('value')
+if(content){
+  quill.setContents(JSON.parse(content));
+}
+
+InsertImage = (image) => {
+  var data = new FormData();
+  data.append('image', image)
+  // Send form request
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    // If image upload Successed
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        var {index, range} = quill.getSelection();
+        quill.insertEmbed(index, 'image', `/uploads/${xhr.responseText}`)
+        index = index + 1;
+        quill.setSelection(index, range)
+      } else {
+        alert(xhr.responseText)
+      }
+    }
+  };
+  xhr.open("PUT", '/api/image/' + `${image.name}_${image.lastModified}`);
+  xhr.send(data);
+}
+
+DeleteImage = (image) => {
+
+  // Send form request
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        console.log('Successfully deleted!')
+      } else {
+        alert(xhr.responseText)
+      }
+    }
+  };
+  xhr.open('DELETE', '/api/image/' + image);
+  xhr.send();
+}
+
+quill.on('text-change', (delta, oldDelta, source) => {
+  if (source == 'user') {
+    var current = quill.getContents()
+    if(delta.ops.some(x => x.delete !== undefined)){
+      var deleted = current.diff(oldDelta)
+      deleted.ops.map(x => {
+        if(x.insert && x.insert.image){
+          deletedImg = x.insert.image;
+          DeleteImage(deletedImg.substr(9))
+        }
+      })
+    }
+  }
+});
+
+// Custom image handler for image upload
+quill.getModule('toolbar').addHandler('image', () => {
+  // Create input for file selection
+  var input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon')
+  input.click()
+
+  input.onchange = () => {
+    InsertImage(input.files[0])
+  };
+})
 
 var form = document.querySelector('form');
 form.onsubmit = function() {
