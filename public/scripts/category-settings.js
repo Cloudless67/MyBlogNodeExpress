d3d712b8cc46335ec59dff0f9af33217688a1ed5
Cloudless67 <cloudlesslakitu67@gmail.com>
2020-08-25 14:09:11 +0900

    category edit

---
+++ public/scripts/category-settings.js
@@ -0,0 +1,25 @@
+var editingCategory = false;
+
+document.getElementById('category-settings').onclick = () => {
+    let add = document.getElementById('category-add');
+    if(add.style.visibility === 'hidden')
+        add.style.visibility = 'visible';
+    else
+        add.style.visibility = 'hidden';
+}
+
+document.getElementById('category-add-submit').onclick = () => {
+    let addText = document.getElementById('category-add-text').value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', `/api/category/${addText}`);
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4){
+        if(xhr.status === 200){
+        }
+        else{
+          alert('Category add failed.');
+        }
+      }
+    }
+    xhr.send();
+}
